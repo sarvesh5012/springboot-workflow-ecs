950fb7278831611d980765c4bfd1988d7997c1eb
sarvesh5012 <114138465+sarvesh5012@users.noreply.github.com>
2023-06-15 14:47:34 +0530

    Update SarveshApplication.java

--- src/main/java/com/example/sarvesh/SarveshApplication.java
+++ src/main/java/com/example/sarvesh/SarveshApplication.java
@@ -14,7 +14,7 @@ public class SarveshApplication {
 	@GetMapping("/")
 
 	public String home(){
-		return "Welcome to aws from jenkins";
+		return "Welcome to aws from jenkins with new commit";
 	}
 
 
