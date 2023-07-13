163aa08c3f77e2c1219926d6c54ab17a2130b093
Manthan-Thakkar <62873934+Manthan-Thakkar@users.noreply.github.com>
2023-07-13 19:07:29 +0530

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ export LOGS_FILE_LOCATION=logs
 envsubst < /etc/td-agent/td-agent-template.conf > /etc/td-agent/td-agent.conf
 cat /etc/td-agent/td-agent.conf
 sudo systemctl start td-agent.service
-curl ${ECS_CONTAINER_METADATA_URI_V4}
+export CLUSTER_ARN=`curl ${ECS_CONTAINER_METADATA_URI_V4} | jq '.Labels."com.amazonaws.ecs.cluster"'`
 java -jar target/mavenwebapp.jar > /logs.log 2>&1
