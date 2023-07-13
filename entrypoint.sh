55d355964d2b3cb6ed0eff17412254c3ecefa065
Manthan-Thakkar <62873934+Manthan-Thakkar@users.noreply.github.com>
2023-07-13 17:59:27 +0530

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ env
 export APP_NAME=JavaApp
 export NEWRELIC_KEY=a844635132d2d9a07559401d9ac7261b62cdNRAL
 export LOGS_FILE_LOCATION=logs
-envsubst < td-agent-template.conf > /etc/td-agent/td-agent.conf
+envsubst < /td-agent-template.conf > /etc/td-agent/td-agent.conf
 curl ${ECS_CONTAINER_METADATA_URI_V4}
 java -jar target/mavenwebapp.jar > /logs.log 2>&1
