aebda33a3c2e13c93633f8e65201d43825b73dd6
Manthan-Thakkar <62873934+Manthan-Thakkar@users.noreply.github.com>
2023-07-13 18:09:02 +0530

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-sudo systemctl start td-agent.service
 env
 export APP_NAME=JavaApp
 export NEWRELIC_KEY=a844635132d2d9a07559401d9ac7261b62cdNRAL
 export LOGS_FILE_LOCATION=logs
-envsubst < /td-agent-template.conf > /etc/td-agent/td-agent.conf
+envsubst < /etc/td-agent/td-agent-template.conf > /etc/td-agent/td-agent.conf
+sudo systemctl start td-agent.service
 curl ${ECS_CONTAINER_METADATA_URI_V4}
 java -jar target/mavenwebapp.jar > /logs.log 2>&1
