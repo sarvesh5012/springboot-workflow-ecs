d1219a0b847a747479da9347e4f0e1315f82ef01
Manthan-Thakkar <62873934+Manthan-Thakkar@users.noreply.github.com>
2023-07-17 12:54:12 +0530

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 env
-export APP_NAME=JavaApp
+export APP_NAME="JavaApp"
 export NEWRELIC_KEY=a844635132d2d9a07559401d9ac7261b62cdNRAL
 export LOGS_FILE_LOCATION=logs
 export CLUSTER_NAME=`curl ${ECS_CONTAINER_METADATA_URI_V4}/task | jq .Cluster`
@@ -8,4 +8,4 @@ export TASK_REVISION=`curl ${ECS_CONTAINER_METADATA_URI_V4}/task | jq .Revision`
 envsubst < /etc/td-agent/td-agent-template.conf > /etc/td-agent/td-agent.conf
 cat /etc/td-agent/td-agent.conf
 sudo systemctl start td-agent.service
-java -jar target/mavenwebapp.jar > /logs.log 2>&1
+java -jar target/mavenwebapp.jar 2>&1 | tee -a /logs.log
